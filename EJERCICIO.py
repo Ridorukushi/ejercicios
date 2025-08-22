555da02bae6fc285aa1832670e5b44544820adbb
Ridorukushi <huver_olayame@fet.edu.co>
2025-08-21 23:42:12 -0500

    hello

--- EJERCICIO.py
+++ EJERCICIO.py
@@ -1,35 +1,35 @@
-def revision_destartalado():
+def revision():
     while True: 
-        print("¿Hola, cómo está? necesita que le colabore con algo?")
+        print("Nesesita que le colabore")
         rta = input("RESPONDA: SI o NO: ")
         
         while rta.strip().lower() not in ("si", "no"):
-            print("No le entiendo, si o no??")
+            print("solo, si o no??")
             rta = input("RESPONDA: SI o NO: ").strip().lower()
 
         if rta.strip().lower() == "si":
-            print("Ok, cuéntame qué sucede con el destartalado. \n") 
+            print("Ok, cuéntame qué sucede con el vehiculo \n") 
             opciones = "1. El carro no arranca. 2. El carro arranca pero se apaga al acelerar. 3. El carro arranca pero el color del humo es... "
             print(opciones)
-            respuesta = input("Vea, lo que pasa es que: ")
+            respuesta = input("el vehiculo tene esto: ")
 
             if respuesta == "1":  
-                print("Las luces del tablero están apagadas o prendidas?")
+                print("Pero las luces del tablero están apagadas o prendidas?")
                 luces_tablero = input("¿Apagadas o Prendidas? Responda. ").strip().lower()
                 if luces_tablero == "prendidas":
-                    print("\n Ahhhh no, eso es pq tiene un fallo en el motor de arranque ")
+                    print("\n lo que tiene es un fallo en el motor de arranque ")
                 elif luces_tablero == "apagadas":
-                    print("\n Ahhhh no, eso es pq tiene la batería descargada ")
+                    print("\n lo que tiene es pq tiene la batería descargada ")
 
             elif respuesta == "2":
-                print("\n Ahhhh no, eso es pq tiene un problema en el suministro de combustible ")
+                print("\n lo que tiene es pq tiene un problema en el suministro de combustible ")
 
             elif respuesta == "3":
                 color_humo = input("¿Blanco o Negro? Responda. ").strip().lower()
                 if color_humo == "negro":
-                    print("\n Ahhhh no, lo más seguro es que eso sea pq tiene una buena mezcla de combustible ")
+                    print("\n  lo que tiene es una mezcla rica en combustible ")
                 elif color_humo == "blanco":
-                    print("\n Ahhhh no, lo más probable es que eso sea pq tiene una falla en la junta de culata ")
+                    print("\n lo tiene es una falla en la junta de culata ")
 
         elif rta.strip().lower() == "no":
             print("ADIOS")
@@ -41,5 +41,5 @@ def revision_destartalado():
             print("ADIOS")
             break  
 
-revision_destartalado()
+revision()
 
